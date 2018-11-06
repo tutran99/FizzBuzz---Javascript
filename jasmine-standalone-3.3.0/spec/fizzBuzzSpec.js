955188959e98013bd4c8dcb64e5cff3baedea284
Tu Tran <tutran@Tus-MacBook-Pro.local>
2018-11-06 14:55:07 +0000

    Add test for fizz and buzz

--- jasmine-standalone-3.3.0/spec/fizzBuzzSpec.js
+++ jasmine-standalone-3.3.0/spec/fizzBuzzSpec.js
@@ -5,9 +5,25 @@ describe('FizzBuzz', function() {
     fizzBuzz = new fizzBuzz()
   });
 
-  describe('fizz for 3', function() {
-    it('can be divided by 3', function() {
+  describe('divisible by 3', function() {
+    it('returns fizz for 3', function() {
+      expect(fizzBuzz.num(3)).toEqual('Fizz');
+    });
 
+    it('returns fizz for 12', function() {
+      expect(fizzBuzz.num(12)).toEqual('Fizz');
     });
   });
+
+  describe('divisible by 5', function() {
+    it('returns buzz for 5', function() {
+      expect(fizzBuzz.num(5)).toEqual('Buzz');
+    });
+
+    it('returns buzz for 25', function() {
+      expect(fizzBuzz.num(25)).toEqual('Buzz');
+    });
+  });
+  
+
 });
