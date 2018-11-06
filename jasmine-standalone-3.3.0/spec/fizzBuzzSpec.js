396bc84c6dcd9051a481078ddc6dae52325b730c
Tu Tran <tutran@Tus-MacBook-Pro.local>
2018-11-06 14:29:58 +0000

    First commit

---
+++ jasmine-standalone-3.3.0/spec/fizzBuzzSpec.js
@@ -0,0 +1,13 @@
+describe('FizzBuzz', function() {
+  var fizzBuzz;
+
+  beforeEach(function() {
+    fizzBuzz = new fizzBuzz()
+  });
+
+  describe('fizz for 3', function() {
+    it('can be divided by 3', function() {
+
+    });
+  });
+});
